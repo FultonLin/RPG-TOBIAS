59e945b894a76662e887f3c77de7fddec0fcdc72
huangtoby <thuang7616@bths.edu>
2017-05-08 14:04:42 -0400

    a

--- Rpg/src/idk/Yuusha.java
+++ Rpg/src/idk/Yuusha.java
@@ -2,22 +2,44 @@ package idk;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
-import guiPractice.components.Moving;
+import guiPractice.Screen;
+import guiPractice.components.Action;
+import guiPractice.components.Graphic;
+import guiPractice.components.Visible;
 
-public class Yuusha extends Moving implements KeyListener{	
+public class Yuusha extends Entity implements KeyListener{	
+	
+	public Action action;
+	private Graphic yuusha;
 
-	public Yuusha(int x, int y, int w, int h) {
-		super(x, y, w, h);
+	public Yuusha(int width, int height) {
+		super(width, height);
+	}
+	
+	public void initObjects(ArrayList<Visible> viewObjects) {
+		yuusha = new Graphic(450,40,.4,"resources/sampleImages/Teemo.jpg");
+		viewObjects.add(yuusha);
 	}
 	
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
             System.out.println("Right key pressed");
+            yuusha.setX(yuusha.getX()+10);
         }
         if (e.getKeyCode() == KeyEvent.VK_LEFT) {
             System.out.println("Left key pressed");
-        }	
+            yuusha.setX(yuusha.getX()-10);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            System.out.println("Up key pressed");
+            yuusha.setX(yuusha.getY()+10);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            System.out.println("Down key pressed");
+            yuusha.setX(yuusha.getY()-10);
+        }
 	}
 
 	@Override
