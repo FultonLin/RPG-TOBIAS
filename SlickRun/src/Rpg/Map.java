fe11d7320e7c90c8562a93f468245df48674ae0b
huangtoby <thuang7616@bths.edu>
2017-05-10 13:42:41 -0400

    a

--- SlickRun/src/Rpg/Map.java
+++ SlickRun/src/Rpg/Map.java
@@ -2,35 +2,47 @@ package Rpg;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Map extends BasicGameState{
 	
+	Image hero;
+	int heroX = 100;
+	int heroY = 100;
+	
 	public Map(int state){
 		
 	}
 
-	@Override
-	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
-		// TODO Auto-generated method stub
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		hero = new Image("resources/Teemo.jpg");
+	}
+
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+		g.drawImage(hero,heroX,heroY);
 		
 	}
 
-	@Override
-	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) throws SlickException {
-		// TODO Auto-generated method stub
-		
+	public void update(GameContainer gc, StateBasedGame sbg, int a) throws SlickException {
+		Input input = gc.getInput();
+		if(input.isKeyDown(Input.KEY_UP)){
+			heroY -= 1;
+		}
+		if(input.isKeyDown(Input.KEY_DOWN)){
+			heroY += 1;
+		}
+		if(input.isKeyDown(Input.KEY_LEFT)){
+			heroX -= 1;
+		}
+		if(input.isKeyDown(Input.KEY_RIGHT)){
+			heroX += 1;
+		}	
 	}
 
-	@Override
-	public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public int getID() {
 		// TODO Auto-generated method stub
 		return 0;
--- SlickRun/src/Rpg/Menu.java
+++ SlickRun/src/Rpg/Menu.java
@@ -13,19 +13,19 @@ public class Menu extends BasicGameState{
 	}
 
 	@Override
-	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) throws SlickException {
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException {
+	public void update(GameContainer gc, StateBasedGame sbg, int a) throws SlickException {
 		// TODO Auto-generated method stub
 		
 	}
