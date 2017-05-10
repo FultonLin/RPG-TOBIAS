9c5d84215446427d07350a03b622eea285f2a444
FultonLin <Flin5289@bths.edu>
2017-05-10 14:29:11 -0400

    Update RPG.java

--- SlickRun/src/Rpg/RPG.java
+++ SlickRun/src/Rpg/RPG.java
@@ -9,18 +9,18 @@ import org.newdawn.slick.state.StateBasedGame;
 public class RPG extends StateBasedGame{
 
 	public static final String gameName = "SlickRun";
-	public static final int map = 0;
-	public static final int menu = 1;
+	public static final int menu = 0;
+	public static final int map = 1;
 	
 	public RPG(String gameName) {
 		super(gameName);
-		this.addState(new Map(map));
 		this.addState(new Menu(menu));
+		this.addState(new Map(map));
 	}
 	
 	@Override
-	public void initStatesList(GameContainer arg0) throws SlickException {
-		// TODO Auto-generated method stub
-		
+	public void initStatesList(GameContainer gc) throws SlickException {
+		this.getState(menu).init(gc,this);
+		this.getState(nap).inti(gc,this);
 	}
 }
