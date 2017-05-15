c795d67d02269b17a95ca2acf3133e702f9424ae
huangtoby <thuang7616@bths.edu>
2017-05-15 13:13:24 -0400

    moving hero

--- SlickRun/src/Rpg/Play.java
+++ SlickRun/src/Rpg/Play.java
@@ -7,57 +7,60 @@ import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
-
-import idk.Animation;
+import org.newdawn.slick.Animation;
 
 public class Play extends BasicGameState{
-	Image hero;
-	int heroX = 100;
-	int heroY = 100;
-	Animation yuusha,moveUp,moveDown,moveLeft,moveRight;
-	int[] animationFrame = {200,200};
-	double yuushaPositionX = 0;
-	double yuushaPositionY = 0;
-	double shiftX = yuushaPositionX+100;
-	double shiftY = yuushaPositionY+100;
 	
-	 
+	Animation yuusha,moveUp,moveDown,moveLeft,moveRight;
+	Image background;
+	boolean quit = false;
+	int[] animationFrame = {200,200};
+	float yuushaPositionX = 0;
+	float yuushaPositionY = 0;
+	float startX = yuushaPositionX+100;
+	float startY = yuushaPositionY+100;
+	
 	public Play(int state) {
 		
 	}
 
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-		hero = new Image("resources/Teemo.jpg");
-		Image[] walkUp = {new Image(""), new Image("")};
-		Image[] walkDown = {new Image(""), new Image("")};
-		Image[] walkLeft = {new Image(""), new Image("")};
-		Image[] walkRight = {new Image(""), new Image("")};
+		background = new Image("resources/shop.png");
+		Image[] walkUp = {new Image("resources/Potion1.png"), new Image("resources/Potion1.png")};
+		Image[] walkDown = {new Image("resources/Potion2.png"), new Image("resources/Potion2.png")};
+		Image[] walkLeft = {new Image("resources/Potion3.png"), new Image("resources/Potion3.png")};
+		Image[] walkRight = {new Image("resources/Potion4.png"), new Image("resources/Potion4.png")};
 		
-		movingUp = new Animation(walkUp,animationFrame,false);
-		movingDown = new Animation(walkDown,animationFrame,false);
-		movingLeft = new Animation(walkLeft,animationFrame,false);
-		movingRight = new Animation(walkRight,animationFrame,false);
-		hero = movingDown;
+		moveUp = new Animation(walkUp,animationFrame,false);
+		moveDown = new Animation(walkDown,animationFrame,false);
+		moveLeft = new Animation(walkLeft,animationFrame,false);
+		moveRight = new Animation(walkRight,animationFrame,false);
+		yuusha = moveDown;
 	}
 
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
-		g.drawImage(hero,heroX,heroY);
-		 
+		background.draw(yuushaPositionX,yuushaPositionY);
+		yuusha.draw(startX,startY);		
+		g.drawString("X: "+yuushaPositionX+"\nY: "+yuushaPositionY,400,200);
 	}
 
 	public void update(GameContainer gc, StateBasedGame sbg, int a) throws SlickException {
 		Input input = gc.getInput();
 		if(input.isKeyDown(Input.KEY_UP)){
-			heroY -= 1;
+			yuusha = moveUp;
+			yuushaPositionY += a*.1f;
 		}
 		if(input.isKeyDown(Input.KEY_DOWN)){
-			heroY += 1;
+			yuusha = moveDown;
+			yuushaPositionY -= a*.1f;
 		}
 		if(input.isKeyDown(Input.KEY_LEFT)){
-			heroX -= 1;
+			yuusha = moveLeft;
+			yuushaPositionX += a*.1f;
 		}
 		if(input.isKeyDown(Input.KEY_RIGHT)){
-			heroX += 1;
+			yuusha = moveRight;
+			yuushaPositionX -= a*.1f;
 		}	
 	}
 
