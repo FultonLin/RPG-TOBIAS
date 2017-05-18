117476e37ad517fefa75506646637cc86ed2bac0
FultonLin <Flin5289@bths.edu>
2017-05-18 14:28:58 -0400

    Update Item.java

--- SlickRun/src/rpg/Item.java
+++ SlickRun/src/rpg/Item.java
@@ -4,11 +4,22 @@ import java.util.ArrayList;
 
 public class Item implements ItemInterface{
 	
+	private String name;
+	private String[] properties;
 
-	@Override
-	public Item getItem() {
-		
-		return null;
+	public Item(String name,String[] properties);
+		this.name = name;
+		this.properties = properties;
 	}
-
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	public String[] properties(){
+		return properties;
+	}
+	
+	
 }
