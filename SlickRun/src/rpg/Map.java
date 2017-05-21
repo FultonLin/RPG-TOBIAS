5393357712bea7d5f21f1221d30e8f01c5993efa
Toby <Toby@Toby-PC.nyc.rr.com>
2017-05-21 19:58:15 -0400

    working on tiledmap

---
+++ SlickRun/src/rpg/Map.java
@@ -0,0 +1,35 @@
+package rpg;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.tiled.TiledMap;
+
+public class Map extends BasicGameState {
+	
+	private TiledMap map;
+
+	public Map(int state) {
+		
+	}
+
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		map = new TiledMap("resources/TiledMap/rpgmap.tmx");
+	}
+
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+		map.render(0,0);
+	}
+
+	public void update(GameContainer gc, StateBasedGame sbg, int a) throws SlickException {
+		int objectLayer = map.getLayerIndex("Object Layer 1");
+		
+		map.getTileId(0,0,objectLayer);
+	}
+
+	public int getID() {
+		return 0;
+	}
+}
