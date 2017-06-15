d75008742fe8af7bfbaf9d7790eec09dba326d11
FultonLin <Flin5289@bths.edu>
2017-06-15 14:11:12 -0400

    Update MonsterInterface.java

--- SlickRun/src/rpg/MonsterInterface.java
+++ SlickRun/src/rpg/MonsterInterface.java
@@ -4,10 +4,4 @@ public interface MonsterInterface {
 	public int gethp();
 	public int getdmg();
 	public void decreaseHp(int dmg);
-	public int getSpd();
-//	public int getxpos();
-//	public int getypos();
-//	public int getWidth();
-//	public int getHeight();
-//	public int getAttkSpd();
 }
